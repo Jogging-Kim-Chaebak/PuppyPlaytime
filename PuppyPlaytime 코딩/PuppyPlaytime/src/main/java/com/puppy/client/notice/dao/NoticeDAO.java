0b5ed045f47f81c632cd33a6316a69792c74a2d7
김주현 <fnqllove11@naver.com>
2022-01-05 13:03:19 +0900

    김주현 사용자 공지사항 페이징기능 병합

--- PuppyPlaytime 코딩/PuppyPlaytime/src/main/java/com/puppy/client/notice/dao/NoticeDAO.java
+++ PuppyPlaytime 코딩/PuppyPlaytime/src/main/java/com/puppy/client/notice/dao/NoticeDAO.java
@@ -2,17 +2,17 @@ package com.puppy.client.notice.dao;
 
 import java.util.List;
 
-import com.puppy.client.notice.domain.PageRequest;
 import com.puppy.client.notice.vo.NoticeVO;
+import com.puppy.common.vo.PageRequest;
 
 public interface NoticeDAO {
-	//글 목록
-	public List<NoticeVO> noticeList() throws Exception;
+	//글 목록(삭제)
+	//public List<NoticeVO> noticeList() throws Exception;
 	//글 상세페이지
 	public NoticeVO noticeDetail(NoticeVO nvo) throws Exception;
 	//게시글 전체 건수를 반환
 	public int count() throws Exception;
 	//페이징 요청 정보를 매개 변수로 받아 페이징 처리를 한 게시글 목록을 반환한다.
-	public List<NoticeVO> list(PageRequest pageRequest) throws Exception;
+	public List<NoticeVO> noticeList(PageRequest pageRequest) throws Exception;
 
 }
