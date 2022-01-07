90f8ea04a7db9974fd8d5f240321c555e1320dc1
KImJuYoung <robolab1902@naver.com>
2022-01-07 15:44:47 +0900

    룸리스트 수정

--- PuppyPlaytime 코딩/PuppyPlaytime/src/main/java/com/puppy/client/member/dao/MemberDaoImpl.java
+++ PuppyPlaytime 코딩/PuppyPlaytime/src/main/java/com/puppy/client/member/dao/MemberDaoImpl.java
@@ -10,48 +10,47 @@ import com.puppy.client.member.vo.MemberVO;
 @Repository
 public class MemberDaoImpl implements MemberDao {
 
-	@Inject
-	private SqlSession session;
+   @Inject
+   private SqlSession session;
 
-	private static final String namespace = "com.puppy.client.member.dao.MemberDao";
+   private static final String namespace = "com.puppy.client.member.dao.MemberDao";
 
-	// 회원가입 처리
-	@Override
-	public void insert(MemberVO mvo) throws Exception {
-		session.insert(namespace + ".insert", mvo);
+   // 회원가입 처리
+   @Override
+   public void insert(MemberVO mvo) throws Exception {
+      session.insert(namespace + ".insert", mvo);
 
-	}
+   }
 
-	// 아이디 중복체크
-	@Override
-	public MemberVO m_idConfirm(String id) throws Exception {
-		return session.selectOne(namespace + ".m_idConfirm", id);
-	}
+   // 아이디 중복체크
+   @Override
+   public MemberVO m_idConfirm(String id) throws Exception {
+      return session.selectOne(namespace + ".m_idConfirm", id);
+   }
 
-	// 아이디 찾기
-	@Override
-	public MemberVO find_id(String m_email) throws Exception {
-		return (MemberVO) session.selectOne(namespace + ".find_id", m_email);
-	}
+   // 아이디 찾기
+   @Override
+   public MemberVO find_id(String m_email) throws Exception {
+      return (MemberVO) session.selectOne(namespace + ".find_id", m_email);
+   }
 
-	// 아이디와 이메일 확인 메서드
-	@Override
-	public MemberVO find_pw(MemberVO mvo) throws Exception {
-		return (MemberVO) session.selectOne(namespace + ".find_pw", mvo);
-	}
+   // 아이디와 이메일 확인 메서드
+   @Override
+   public MemberVO find_pw(MemberVO mvo) throws Exception {
+      return (MemberVO) session.selectOne(namespace + ".find_pw", mvo);
+   }
 
-	// 비밀번호 수정 페이지
-	@Override
-	public MemberVO read(MemberVO mvo) throws Exception {
-		return (MemberVO) session.selectList(namespace + ".read", mvo);
-	}
+   // 비밀번호 수정 페이지
+   @Override
+   public MemberVO read(MemberVO mvo) throws Exception {
+      return (MemberVO) session.selectOne(namespace + ".read", mvo);
+   }
 
-	// 비밀번호 수정 팝업 페이지
-	@Override
-	public void update(MemberVO mvo) throws Exception {
-		// System.out.println("aaaaaaaaa"+mvo.getM_pw());
-		session.update(namespace + ".update", mvo);
+   // 비밀번호 수정 팝업 페이지
+   @Override
+   public void update(MemberVO mvo) throws Exception {
+      session.update(namespace + ".update", mvo);
 
-	}
+   }
 
 }
